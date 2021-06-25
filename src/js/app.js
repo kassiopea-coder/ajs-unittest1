2f8e411138030cfd8d637a97050dcdcb48907ea7
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-06-25 22:08:02 +0400

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 export default function healthCalc(player) {
-    const status = { healthy: 51, wounded: 15, critical: 0 };
-    return Object.keys(status).find((name) => status[name] <= player.health);
+  const status = { healthy: 51, wounded: 15, critical: 0 };
+  return Object.keys(status).find((name) => status[name] <= player.health);
 }
